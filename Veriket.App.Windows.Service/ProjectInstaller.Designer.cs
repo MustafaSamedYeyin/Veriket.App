fdcda5ef9c6967324eac9aca3133779ab37ce699
Mustafa Samed Yeyin <mustafasamedyeyin@gmail.com>
2025-06-21 13:05:33 +0300

    - Switching to .net framework for service installation with setup.

---
+++ Veriket.App.Windows.Service/ProjectInstaller.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Veriket.App.Windows.Service
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller3 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // serviceInstaller3
+            // 
+            this.serviceInstaller3.DelayedAutoStart = true;
+            this.serviceInstaller3.Description = "VeriketApp Service Test";
+            this.serviceInstaller3.DisplayName = "VeriketApp Service";
+            this.serviceInstaller3.ServiceName = "VeriketApp Service";
+            this.serviceInstaller3.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller3});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller3;
+    }
+}
---
+++ Veriket.App.Windows.Service/Service.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Veriket.App.Shared.Entities;
+using Veriket.App.Shared.Service;
+
+namespace Veriket.App.Windows.Service
+{
+    partial class Service : ServiceBase
+    {
+        private LogService _logService;
+        private CancellationTokenSource _cancellationTokenSource;
+        private Task _executingTask;
+
+        public Service()
+        {
+            InitializeComponent();
+            ServiceName = "VeriketAppService";
+            var logFolderName = ConfigurationManager.AppSettings["VeriketLogSettings:LogFolder"];
+            var logFileName = ConfigurationManager.AppSettings["VeriketLogSettings:LogFileName"];
+            _logService = new LogService(logFolderName, logFileName);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            _executingTask = ExecuteAsync(_cancellationTokenSource.Token);
+        }
+
+        protected override void OnStop()
+        {
+            _cancellationTokenSource.Cancel();
+            _executingTask.Wait();
+        }
+
+        private async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var log = new Log
+            {
+                ComputerName = Environment.MachineName,
+                User = Environment.UserName
+            };
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    _logService.WriteLog(log);
+                }
+                catch (Exception ex)
+                {
+                    EventLog.WriteEntry(ServiceName, ex.ToString(), EventLogEntryType.Error);
+                }
+
+                await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+            }
+        }
+    }
+}
